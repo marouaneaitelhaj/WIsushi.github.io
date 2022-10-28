6a846d0dff9e17e327c9b2748c1d03e2cd1f00d9
marouaneaitelhaj <112936725+marouaneaitelhaj@users.noreply.github.com>
2022-10-28 22:30:37 +0100

    update

--- Wi sushi/carte.js
+++ Wi sushi/carte.js
@@ -3,11 +3,9 @@ for (var i = 0; i < btnBuyPlus.length; i++){
     var first = btnBuyPlus[i];
     first.addEventListener("click", function(event) {
         var secend = event.target;
-        var third = secend.parentElement.parentElement.parentElement.children[3].children;
-        var fourth = third[0].value;
-        var newvalue = parseInt(fourth) + 1;
-        console.log(newvalue);
-        third[0].value = newvalue
+        var third = secend.parentElement.parentElement.parentElement.children[2].children[1];
+        var fourth = parseInt(third.value);
+        third.value = fourth + 1;
     })
 }
 var btnBuyMinus = document.getElementsByClassName("btn-buy-minus");
@@ -15,19 +13,49 @@ for (var i = 0; i < btnBuyMinus.length; i++){
     var first = btnBuyMinus[i];
     first.addEventListener("click", function(event) {
         var secend = event.target;
-        var third = secend.parentElement.parentElement.parentElement.children[3].children;
-        var fourth = third[0].value;
-        if (parseInt(fourth) > 1){
-        let newvalue = parseInt(fourth) - 1;
-        console.log(newvalue);
-        third[0].value = newvalue;
+        var third = secend.parentElement.parentElement.parentElement.children[2].children[1];
+        var fourth = parseInt(third.value);
+        if (third.value >= 1){
+        third.value = fourth - 1;
     }
         
     })
 }
-
-var doneprice = document.querySelector(".done-pricz");
+var btnbuybuy = document.querySelector(".x-close");
 var carddetail = document.querySelector(".card-details");
-doneprice.addEventListener("click", function(){
+btnbuybuy.addEventListener("click", function(){
     carddetail.classList.toggle("btnshadow");
+})
+document.querySelector("body > div.filterby > div:nth-child(1)").addEventListener("click" , function(){
+    document.querySelector("body > div.carte-countiner.SALADE").style.display = "block";
+    document.querySelector("body > h3:nth-child(4)").style.display = "block";
+    document.querySelector("body > div.carte-countiner.OBENTO").style.display = "none";
+    document.querySelector("body > h3:nth-child(6)").style.display = "none";
+    document.querySelector("body > div.carte-countiner.PLATS").style.display = "none";
+    document.querySelector("body > h3:nth-child(8)").style.display = "none";
+    document.querySelector("body > div.filterby > div:nth-child(1)").style.color = "#ED3C20";
+    document.querySelector("body > div.filterby > div:nth-child(2)").style.color = "black";
+    document.querySelector("body > div.filterby > div:nth-child(3)").style.color = "black";
+})
+document.querySelector("body > div.filterby > div:nth-child(2)").addEventListener("click" , function(){
+    document.querySelector("body > div.carte-countiner.SALADE").style.display = "none";
+    document.querySelector("body > h3:nth-child(4)").style.display = "none";
+    document.querySelector("body > div.carte-countiner.OBENTO").style.display = "block";
+    document.querySelector("body > h3:nth-child(6)").style.display = "block";
+    document.querySelector("body > div.carte-countiner.PLATS").style.display = "none";
+    document.querySelector("body > h3:nth-child(8)").style.display = "none";
+    document.querySelector("body > div.filterby > div:nth-child(2)").style.color = "#ED3C20";
+    document.querySelector("body > div.filterby > div:nth-child(3)").style.color = "black";
+    document.querySelector("body > div.filterby > div:nth-child(1)").style.color = "black";
+})
+document.querySelector("body > div.filterby > div:nth-child(3)").addEventListener("click" , function(){
+    document.querySelector("body > div.carte-countiner.SALADE").style.display = "none";
+    document.querySelector("body > h3:nth-child(4)").style.display = "none";
+    document.querySelector("body > div.carte-countiner.OBENTO").style.display = "none";
+    document.querySelector("body > h3:nth-child(6)").style.display = "none";
+    document.querySelector("body > div.carte-countiner.PLATS").style.display = "block";
+    document.querySelector("body > h3:nth-child(8)").style.display = "block";
+    document.querySelector("body > div.filterby > div:nth-child(3)").style.color = "#ED3C20";
+    document.querySelector("body > div.filterby > div:nth-child(2)").style.color = "black";
+    document.querySelector("body > div.filterby > div:nth-child(1)").style.color = "black";
 })
--- Wi sushi/main.js
+++ Wi sushi/main.js
@@ -3,5 +3,34 @@ var menu = document.querySelector("ul");
 logo.addEventListener("click", function(){
     menu.classList.toggle("active");
 })
-var string = document.querySelector(".first-price-display");
-var matches = string.textContent.match(/(\d+)/);
+
+var quantity = document.querySelectorAll(".inpt-quantiti");
+/*var btnbuybuy = document.querySelector(".done-pricz");
+var carddetail = document.querySelector(".card-details");*/
+var listBuyButton = document.querySelectorAll(".done-pricz");
+for (var i = 0; i < listBuyButton.length; i++){
+    var first = listBuyButton[i];
+    first.addEventListener("click", function(event){
+        var secend = event.target;
+        var third = secend.parentElement.parentElement.parentElement.parentElement.parentElement.children[1];
+        third.classList.toggle("btnshadow");
+        var five = secend.parentElement.parentElement.children[2].children[1];
+        var quantityNumber = five.value;
+        var six = secend.parentElement.parentElement.children[2].children[0];
+        var priceNumber = six.textContent.match(/(\d+)/);
+        var total = parseInt(priceNumber) * quantityNumber;
+        document.querySelector("#food-frice").textContent = total + "$";
+        document.querySelector("body > div.card-details.btnshadow > div:nth-child(5) > p:nth-child(2)").textContent = (total + 3) + "$";
+    }
+    
+    
+    )
+}
+
+
+
+
+
+
+
+
