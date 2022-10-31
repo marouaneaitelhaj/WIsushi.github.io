aa7cf602b0125e5a4ac2bdc430fd4cb2b5d5dc7e
marouaneaitelhaj <112936725+marouaneaitelhaj@users.noreply.github.com>
2022-10-31 17:51:13 +0100

    version finale

--- main.js
+++ main.js
@@ -8,43 +8,51 @@ logo.addEventListener("click", function(){
 var quantity = document.querySelectorAll(".inpt-quantiti");
 /*var btnbuybuy = document.querySelector(".done-pricz");
 var carddetail = document.querySelector(".card-details");*/
-var listBuyButton = document.querySelectorAll(".done-pricz");
-for (var i = 0; i < listBuyButton.length; i++){
-    var first = listBuyButton[i];
-    first.addEventListener("click", function(event){
-        var secend = event.target;
-        var third = secend.parentElement.parentElement.parentElement.parentElement.parentElement.children[1];
-        third.classList.toggle("btnshadow");
-        var five = secend.parentElement.parentElement.children[2].children[1];
-        var quantityNumber = five.value;
-        var six = secend.parentElement.parentElement.children[2].children[0];
-        var priceNumber = six.textContent.match(/(\d+)/);
-        var total = parseInt(priceNumber) * quantityNumber;
-        document.querySelector("#food-frice").textContent = total + "$";
-        document.querySelector("body > div.card-details.btnshadow > div:nth-child(5) > p:nth-child(2)").textContent = (total + 3) + "$";
 
-
-        document.querySelector("body > div.card-details.btnshadow > div:nth-child(7) > div:nth-child(2) > button").addEventListener("click", function(){
-
-            numberofachattime++;
-        console.log(numberofachattime);
-        document.querySelector("body > i > sup").textContent = numberofachattime;
-
-
-
-
-        document.querySelector(".big-archive").cloneNode(true);
-        document.querySelector("body > div.archive").appendChild(document.querySelector(".big-archive").cloneNode(true));
-        
-        document.querySelector("body > div.archive > div > div:nth-child(1)").textContent = secend.parentElement.parentElement.children[1].children[0].textContent;
-        document.querySelector("body > div.archive > div > div:nth-child(2)").textContent = "-" + total + "$";
-        
-        carddetail.classList.toggle("btnshadow");
-        })
+var total = 0;
+var minitotal = 0;
+document.querySelector(".done-pricz").addEventListener("click", function(){
+    for (var  i = 0; i < document.querySelectorAll(".inpt-quantiti").length; i++){
+        minitotal  = document.querySelectorAll(".inpt-quantiti")[i].value;
+        var price = document.querySelectorAll(".first-price-display")[i].textContent.match(/(\d+)/);
+        total =  total + parseInt(price) * minitotal;
         
     }
-    )
+    console.log(minitotal);
+    
+    document.querySelector(".card-details").classList.toggle("btnshadow");
+    document.querySelector(".card-details").children[2].children[1].textContent = total + "$";
 }
+)
+
+
+
 document.querySelector("body > i").addEventListener("click" , function(){
     document.querySelector(".archive").classList.toggle("btnshadow");
+})
+
+
+
+
+
+
+
+
+
+document.querySelector("body > div.card-details.btnshadow > div:nth-child(7) > div:nth-child(2) > button").addEventListener("click", function(){
+
+    numberofachattime++;
+console.log(numberofachattime);
+document.querySelector("body > i > sup").textContent = numberofachattime;
+
+
+
+
+document.querySelector(".big-archive").cloneNode(true);
+document.querySelector("body > div.archive").appendChild(document.querySelector(".big-archive").cloneNode(true));
+
+document.querySelector("body > div.archive > div > div:nth-child(1)").textContent = secend.parentElement.parentElement.children[1].children[0].textContent;
+document.querySelector("body > div.archive > div > div:nth-child(2)").textContent = "-" + total + "$";
+
+carddetail.classList.toggle("btnshadow");
 })
