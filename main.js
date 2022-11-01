e2ac9032de9808fffc4fef46abd84ede7b74e415
marouaneaitelhaj <112936725+marouaneaitelhaj@users.noreply.github.com>
2022-11-01 09:58:23 +0100

    last version

--- main.js
+++ main.js
@@ -15,13 +15,20 @@ document.querySelector(".done-pricz").addEventListener("click", function(){
     for (var  i = 0; i < document.querySelectorAll(".inpt-quantiti").length; i++){
         minitotal  = document.querySelectorAll(".inpt-quantiti")[i].value;
         var price = document.querySelectorAll(".first-price-display")[i].textContent.match(/(\d+)/);
-        total =  total + parseInt(price) * minitotal;
+        total +=  parseInt(price) * minitotal;
+        
         
     }
     console.log(minitotal);
     
     document.querySelector(".card-details").classList.toggle("btnshadow");
     document.querySelector(".card-details").children[2].children[1].textContent = total + "$";
+    document.querySelector("body > div.card-details.btnshadow > div:nth-child(5) > p:nth-child(2)").textContent = 3 + total + "$";
+    for (var  i = 0; i < document.querySelectorAll(".inpt-quantiti").length; i++){
+        document.querySelectorAll(".inpt-quantiti")[i].value = 0;
+        total = 0;
+        
+    }
 }
 )
 
@@ -40,19 +47,19 @@ document.querySelector("body > i").addEventListener("click" , function(){
 
 
 document.querySelector("body > div.card-details.btnshadow > div:nth-child(7) > div:nth-child(2) > button").addEventListener("click", function(){
-
     numberofachattime++;
-console.log(numberofachattime);
-document.querySelector("body > i > sup").textContent = numberofachattime;
-
-
-
-
-document.querySelector(".big-archive").cloneNode(true);
-document.querySelector("body > div.archive").appendChild(document.querySelector(".big-archive").cloneNode(true));
-
-document.querySelector("body > div.archive > div > div:nth-child(1)").textContent = secend.parentElement.parentElement.children[1].children[0].textContent;
-document.querySelector("body > div.archive > div > div:nth-child(2)").textContent = "-" + total + "$";
+    console.log(numberofachattime);
+    document.querySelector("body > i > sup").textContent = numberofachattime;
+    
+    
+    
+    
+    document.querySelector(".big-archive").cloneNode(true);
+    document.querySelector("body > div.archive").appendChild(document.querySelector(".big-archive").cloneNode(true));
+    
+    document.querySelector("body > div.archive > div > div:nth-child(1)").textContent = secend.parentElement.parentElement.children[1].children[0].textContent;
+    document.querySelector("body > div.archive > div > div:nth-child(2)").textContent = "-" + total + "$";
+    total = 0;
 
 carddetail.classList.toggle("btnshadow");
 })
