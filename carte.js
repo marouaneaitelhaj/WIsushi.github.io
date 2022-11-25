4f36c4747f2e738eaa823ef348a60231f0dee17b
marouaneaitelhaj <112936725+marouaneaitelhaj@users.noreply.github.com>
2022-11-25 10:04:24 +0100

    update

--- carte.js
+++ carte.js
@@ -1,12 +1,17 @@
 var btnBuyPlus = document.getElementsByClassName("btn-buy-plus");
+var globalsecend;
+var globalthird;
 for (var i = 0; i < btnBuyPlus.length; i++){
     var first = btnBuyPlus[i];
     first.addEventListener("click", function(event) {
         var secend = event.target;
+        globalsecend = secend;
         var third = secend.parentElement.parentElement.parentElement.children[2].children[1];
         var fourth = parseInt(third.value);
         third.value = fourth + 1;
+        globalthird = third;
     })
+    
 }
 var btnBuyMinus = document.getElementsByClassName("btn-buy-minus");
 for (var i = 0; i < btnBuyMinus.length; i++){
@@ -24,25 +29,37 @@ for (var i = 0; i < btnBuyMinus.length; i++){
 var btnbuybuy = document.querySelector(".x-close");
 var carddetail = document.querySelector(".card-details");
 btnbuybuy.addEventListener("click", function(){
+    document.querySelector(".done-pricz").classList.toggle('active');
     carddetail.classList.toggle("btnshadow");
+    zlayga.innerHTML = "";
 })
 document.querySelector("body > div.filterby > div:nth-child(1)").addEventListener("click", function(){
-    document.querySelector("body > div.carte-countiner.SALADE").style.display = "flex";
-    document.querySelector("body > div.carte-countiner.OBENTO").style.display = "flex";
-    document.querySelector("body > div.carte-countiner.PLATS").style.display = "flex";
+    document.querySelector("body > div.carte-countiner.SALADE").style.display = "grid";
+    document.querySelector("body > div.carte-countiner.OBENTO").style.display = "grid";
+    document.querySelector("body > div.carte-countiner.PLATS").style.display = "grid";
 })
 document.querySelector("body > div.filterby > div:nth-child(2)").addEventListener("click", function(){
-    document.querySelector("body > div.carte-countiner.SALADE").style.display = "flex";
+    document.querySelector("body > div.carte-countiner.SALADE").style.display = "grid";
     document.querySelector("body > div.carte-countiner.OBENTO").style.display = "none";
     document.querySelector("body > div.carte-countiner.PLATS").style.display = "none";
 })
 document.querySelector("body > div.filterby > div:nth-child(3)").addEventListener("click", function(){
     document.querySelector("body > div.carte-countiner.SALADE").style.display = "none";
-    document.querySelector("body > div.carte-countiner.OBENTO").style.display = "flex";
+    document.querySelector("body > div.carte-countiner.OBENTO").style.display = "grid";
     document.querySelector("body > div.carte-countiner.PLATS").style.display = "none";
 })
 document.querySelector("body > div.filterby > div:nth-child(4)").addEventListener("click", function(){
     document.querySelector("body > div.carte-countiner.SALADE").style.display = "none";
     document.querySelector("body > div.carte-countiner.OBENTO").style.display = "none";
-    document.querySelector("body > div.carte-countiner.PLATS").style.display = "flex";
+    document.querySelector("body > div.carte-countiner.PLATS").style.display = "grid";
 })
+
+
+
+document.querySelector("#buynow").addEventListener("click", function(e){
+    for (var  i = 0; i < document.querySelectorAll(".inpt-quantiti").length; i++){
+        document.querySelectorAll(".inpt-quantiti")[i].value = 0;
+    };
+
+})
+
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var logo = document.querySelector(".logo");
 var menu = document.querySelector("ul");
 var numberofachattime = 0;
+var zlayga = document.getElementById("list-done-buy");
 logo.addEventListener("click", function(){
     menu.classList.toggle("active");
 })
@@ -9,26 +10,38 @@ var quantity = document.querySelectorAll(".inpt-quantiti");
 /*var btnbuybuy = document.querySelector(".done-pricz");
 var carddetail = document.querySelector(".card-details");*/
 
-var total = 0;
-var minitotal = 0;
 document.querySelector(".done-pricz").addEventListener("click", function(){
+    var total = 0;
+    // var minitotal = 0;
+    document.querySelector(".done-pricz").classList.toggle('active');
+    zlayga.innerHTML = "";
+    
     for (var  i = 0; i < document.querySelectorAll(".inpt-quantiti").length; i++){
         minitotal  = document.querySelectorAll(".inpt-quantiti")[i].value;
         var price = document.querySelectorAll(".first-price-display")[i].textContent.match(/(\d+)/);
         total +=  parseInt(price) * minitotal;
-        
+
+        if (minitotal > 0){
+            zlayga.innerHTML+=`<div class="p-list-div"><p class="p-list">${document.querySelectorAll(".inpt-quantiti")[i].parentElement.parentElement.children[1].children[0].textContent}   x${minitotal}</p><i class="fa fa-trash dltbtn" aria-hidden="true"></i></div>`;
+        }
         
     }
+    var dltbtn = document.getElementsByClassName("dltbtn");
+    for (var i = 0; i < dltbtn.length; i++){
+        var first = dltbtn[i];
+        first.addEventListener("click", function(event) {
+            event.target.parentElement.remove();
+        })
+    }    
     console.log(minitotal);
     
     document.querySelector(".card-details").classList.toggle("btnshadow");
-    document.querySelector(".card-details").children[2].children[1].textContent = total + "$";
+    document.querySelector(".card-details").children[2].children[2].textContent = total + "$";
     document.querySelector("body > div.card-details.btnshadow > div:nth-child(5) > p:nth-child(2)").textContent = 3 + total + "$";
-    for (var  i = 0; i < document.querySelectorAll(".inpt-quantiti").length; i++){
-        document.querySelectorAll(".inpt-quantiti")[i].value = 0;
-        total = 0;
-        
-    }
+    console.log(document.querySelector("body > div.card-details.btnshadow > div:nth-child(5) > p:nth-child(2)").textContent);
+    
+    total = 0;
+    
 }
 )
 
@@ -36,30 +49,4 @@ document.querySelector(".done-pricz").addEventListener("click", function(){
 
 document.querySelector("body > i").addEventListener("click" , function(){
     document.querySelector(".archive").classList.toggle("btnshadow");
-})
-
-
-
-
-
-
-
-
-
-document.querySelector("body > div.card-details.btnshadow > div:nth-child(7) > div:nth-child(2) > button").addEventListener("click", function(){
-    numberofachattime++;
-    console.log(numberofachattime);
-    document.querySelector("body > i > sup").textContent = numberofachattime;
-    
-    
-    
-    
-    document.querySelector(".big-archive").cloneNode(true);
-    document.querySelector("body > div.archive").appendChild(document.querySelector(".big-archive").cloneNode(true));
-    
-    document.querySelector("body > div.archive > div > div:nth-child(1)").textContent = secend.parentElement.parentElement.children[1].children[0].textContent;
-    document.querySelector("body > div.archive > div > div:nth-child(2)").textContent = "-" + total + "$";
-    total = 0;
-
-carddetail.classList.toggle("btnshadow");
 })
